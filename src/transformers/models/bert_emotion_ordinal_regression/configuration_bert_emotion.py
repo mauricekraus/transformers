e594bb54278b5a0685420920aea11efac1545051
Maurice Kraus <dev@mkraus.io>
2025-02-21 12:27:31 +0100

    Update default bert_config in BertForMultiOutputOrdinalRegressionConfig to use pretrained model

--- src/transformers/models/bert_emotion_ordinal_regression/configuration_bert_emotion.py
+++ src/transformers/models/bert_emotion_ordinal_regression/configuration_bert_emotion.py
@@ -72,7 +72,7 @@ class BertForMultiOutputOrdinalRegressionConfig(PretrainedConfig):
     model_type = "bert-emotion-ordinal-regression"
     def __init__(
         self,
-        bert_config: BertConfig = BertConfig(),
+        bert_config: PretrainedConfig = BertConfig.from_pretrained("bert-base-uncased"),
         categories_mapping: list[tuple[str, float, float]] = CATEGORIES_MAPPING,
         dimensions_mapping: list[tuple[str, float, float]] = DIMENSIONS_MAPPING,
         hidden_dropout_prob: float = 0.1,
